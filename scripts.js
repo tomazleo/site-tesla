98008dc272c4dfec2462f87854695c03a140202d
tomazleo <tomazleozz@gmail.com>
2024-08-19 22:34:46 -0300

    primeiro contato com programação, aulão de site copia da tesla

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "34%"
+}
+
+function desaparecerFormulario(){
+     formulario.style.left = "-60%"
+}
